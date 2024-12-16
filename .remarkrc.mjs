145bb004292bdc11c1e64a2d7cd110628106956c
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-12-16 18:04:52 +0700

    chore: initial commit
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

---
+++ .remarkrc.mjs
@@ -0,0 +1,29 @@
+import remarkFrontmatter from "remark-frontmatter";
+import remarkLintFrontmatterSchema from "remark-lint-frontmatter-schema";
+
+const remarkConfig = {
+  plugins: [
+    [
+      "@davidsneighbour/remark-config",
+      ["remark-lint-write-good", false],
+
+      [
+        "remark-lint-no-undefined-references",
+        {
+          allow: ["!NOTE", "!TIP", "!CAUTION", "!IMPORTANT", "!WARNING"],
+        },
+      ],
+    ],
+    remarkFrontmatter,
+    [
+      remarkLintFrontmatterSchema,
+      {
+        schemas: {
+          "./static/_schemata/blog.schema.yaml": ["./content/blog/**/*.{md,mdx}"],
+        },
+      },
+    ],
+  ],
+};
+
+export default remarkConfig;
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/davidsneighbour/kollitsch.me
+
+go 1.23.4
+
+require (
+	github.com/davidsneighbour/hugo-modules/modules/debug v1.2024.33 // indirect
+	github.com/davidsneighbour/hugo-modules/modules/head v1.2024.23 // indirect
+	github.com/davidsneighbour/hugo-modules/modules/hooks v1.2024.16 // indirect
+)
